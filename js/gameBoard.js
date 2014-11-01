a9b5e870dabc26851ff290b58ceee352943d33ce
Vivyan Woods <dr.v.woods@gmail.com>
2014-11-01 12:11:30 -0700

    started coding for adding images

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,5 +1,13 @@
 //javascript code
 //with jQuery!! :D
+//javascript to make and control the game board
 
 "use strict";
 
+var tile = 'img/sec1.jpg';
+var info = 'some other data';
+
+var game_Board = $('#game-board');
+
+//create and configure a new img element
+var newTile = $(document.createElement('img'));
