7642963c7d1076e21208e3ba1ea1666546d49d76
Vivyan Woods <dr.v.woods@gmail.com>
2014-11-01 12:26:28 -0700

    added interactivity

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -11,3 +11,22 @@ var game_Board = $('#game-board');
 
 //create and configure a new img element
 var newTile = $(document.createElement('img'));
+
+newTile.attr('src', 'img/tile-back.jpg');
+newTile.attr('alt', 'photo of nature');
+
+game_Board.append(newTile);
+
+var allSections = $('section');
+
+function onReady() {
+	allSections.hide();
+}
+
+var allNavLinks = $('nav a');
+
+allNavLinks.click(function  () {
+	$($(this).attr('href')).fadeIn();
+});
+
+$(onReady);
